1cc97cc7e419e3fbb0aff0e853b9239d159cc4f7
Cameron Falkenhagen <music@twosmiles.ca>
2015-04-29 20:38:58 -0600

    More examples

--- kata/events/events.js
+++ kata/events/events.js
@@ -2,3 +2,12 @@ function startApp() {
   // alert('loaded');
   console.log('loaded');
 }
+
+function clicked(e, that) {
+  console.log(e, that.parentElement);
+  return false;
+}
+
+function hover(e, el) {
+  console.log(e,el);
+}
