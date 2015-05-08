0d547dbc1cd94bb69e8bad9c016ddfe6a73dae1f
Cameron Falkenhagen <music@twosmiles.ca>
2015-05-08 07:05:05 -0600

    Add plugins.

---
+++ kata/plugins/index.php
@@ -0,0 +1,10 @@
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet" />
+<link href="https://code.jquery.com/ui/1.11.3/themes/smoothness/jquery-ui.css" rel="stylesheet" />
+<script src="https://code.jquery.com/jquery-2.1.4.js"></script>
+<script src="https://code.jquery.com/ui/1.11.3/jquery-ui.js"></script>
+<script src="plugins.js"></script>
+
+
+<h2>Plugins</h2>
+
+<input type="text" id="date" />
